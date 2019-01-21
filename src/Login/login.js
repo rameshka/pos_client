0926c52824ed50f71621e94e7cad2a63e63236d8
piyumir <rameshkafox@gmail.com>
2019-01-21 17:35:18 +0530

    auth login

--- src/Login/login.js
+++ src/Login/login.js
@@ -1,62 +1,94 @@
-import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./login.css";
+import React, { Component } from 'react';
+import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
+import {setUser} from "../Services/Auth";
+import { createHashHistory } from 'history';
 
+export const history = createHashHistory();
 export default class Login extends Component {
+
     constructor(props) {
         super(props);
-
         this.state = {
-            email: "",
-            password: ""
+            response: '',
+            username:'rameshkafox@gmail.com',
+            password:1234
         };
+
     }
 
-    validateForm() {
-        return this.state.email.length > 0 && this.state.password.length > 0;
-    }
-
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username: this.state.username ,password:this.state.password}),
         });
-    }
+        const body = await response.json();
 
-    handleSubmit = event => {
-        event.preventDefault();
+        if(body.success){
+            setUser(body.token);
+            history.push('/dashboard');
+            //redirect to dashboard view
+        }else{
+            //view error
+        }
+
     }
 
     render() {
         return (
-            <div className="Login">
-                <form onSubmit={this.handleSubmit}>
-                    <FormGroup controlId="username" bsSize="large">
-                        <ControlLabel>Email</ControlLabel>
-                        <FormControl
-                            autoFocus
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                    </FormGroup>
-                    <FormGroup controlId="password" bsSize="large">
-                        <ControlLabel>Password</ControlLabel>
-                        <FormControl
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type="password"
-                        />
-                    </FormGroup>
-                    <Button
-                        block
-                        bsSize="large"
-                        disabled={!this.validateForm()}
-                        type="submit"
-                    >
-                        Login
-                    </Button>
-                </form>
+
+            <div className='login-form'>
+                {/*
+      Heads up! The styles below are necessary for the correct render of this example.
+      You can do same with CSS, the main idea is that all the elements up to the `Grid`
+      below must have a height of 100%.
+    */}
+                <style>{`
+      body > div,
+      body > div > div,
+      body > div > div > div.login-form {
+        height: 100%;
+      }
+    `}</style>
+                <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+                    <Grid.Column style={{ maxWidth: 450 }}>
+                        <Header as='h2' color='teal' textAlign='center'>
+                            Log-in to your account
+                        </Header>
+                        <p>{this.state.response}</p>
+                        <Form size='large' onSubmit={this.handleSubmit}>
+                            <Segment stacked>
+                                <Form.Input
+                                    fluid icon='user'
+                                    iconPosition='left'
+                                    placeholder='E-mail address'
+                                    value={this.state.username}
+                                    onChange={e => this.setState({ username: e.target.value })}
+                                />
+                                <Form.Input
+                                    fluid
+                                    icon='lock'
+                                    iconPosition='left'
+                                    placeholder='Password'
+                                    type='password'
+                                    value={this.state.password}
+                                    onChange={e => this.setState({ password: e.target.value })}
+                                />
+                                <Button color='teal' fluid size='large'>
+                                    Login
+                                </Button>
+                            </Segment>
+                        </Form>
+                        <Message>
+                            New to us? <a href='#'>Sign Up</a>
+                        </Message>
+                    </Grid.Column>
+                </Grid>
             </div>
+
         );
     }
 }
